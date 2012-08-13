de4e2907a39c9789624b38d91be25940fff6da4f
Stephen Irons <stephen.irons@clear.net.nz>
2012-08-13 19:41:02 +1200

    Calculate chord tones.

--- chords.lua
+++ chords.lua
@@ -7,14 +7,20 @@ local name_grammar = re.compile(
 [[
    chord <- { root_note name !.}  -> {}
    
-   name <- {:name: quality ? :} -> {}
-   quality <- 'M' / 'maj' / 'min' / 'aug' / 'dim' / 'm' / '+' / '-' / 'o' / '0' / 'dom'
-   root_note <- {:note: [ABCDEFG] sharp_or_flat? :} -> {}
+   root_note <- {:note: (note_name sharp_or_flat ?) / numeral :} -> {}
+   note_name <- [ABCDEFG]
    sharp_or_flat <- 'b' / '#'
+   numeral <- 'VII' / 'VI' / 'V' / 'IV' / 'III' / 'II' / 'I'
+
+   name <- {:chord: quality ? addition ? :} -> {}
+   quality <- 'M' / 'maj' / 'min' / 'aug' / 'dim' / 'm' / '+' / '-' / 'o' / '0' / 'dom'
+   addition <- seventh
+   seventh <- '7' / 'M7'
 ]])
 
-local chords = {
+local chord_tones_by_name = {
    ['']    = { 0, 4, 7 },
+   ['M']   = { 0, 4, 7 },
    ['m']   = { 0, 3, 7 },
    ['-']   = { 0, 3, 7 },
    ['+']   = { 0, 4, 8 },
@@ -33,22 +39,41 @@ local chords = {
 
 local midi_name = 
 {
-   ['C'] = 60,
-   ['D'] = 62,
-   ['E'] = 64,
-   ['F'] = 65,
-   ['G'] = 67,
-   ['A'] = 69,
-   ['B'] = 71,
+   ['C']  = 60,
+   ['C#'] = 61,
+   ['Db'] = 61,
+   ['D']  = 62,
+   ['E']  = 64,
+   ['F']  = 65,
+   ['G']  = 67,
+   ['A']  = 69,
+   ['B']  = 71,
 }
 
+local function name_parse(name)
+   return re.find(name, name_grammar)
+end
 
+serpent=require('serpent')
 local function make_chord(name, octave)
-   root = string.sub(name, 1, 1)
+   local octave = octave or 4
+   local err, chord = name_parse(name)
+   --print(serpent.block(chord))
+   local root = chord[2].note
+   local name = chord[3].chord
+   --print(root, name)
+   local tones = chord_tones_by_name[name]
+   local octave_offset = (octave+1) * 12
+   for i = 1, #tones do
+       tones[i] = tones[i] + octave_offset
+    end
+   --print(serpent.block(tones))
+   return tones
 end
 
 M.chords = chords
-M.parse = function(name) return re.find(name, name_grammar) end
+M.parse = name_parse
+M.make_chord = make_chord
 
 return M
 
--- test/test_chords.lua
+++ test/test_chords.lua
@@ -19,7 +19,7 @@ function Test_chords:test_bad_chord()
    assertEquals(chord, nil)
 end
 
-local good_notes = 
+local notes = 
 {
    'C' , 'D' , 'E' , 'F' , 'G' , 'A' , 'B' ,
    'C#', 'D#', 'E#', 'F#', 'G#', 'A#', 'B#',
@@ -27,9 +27,23 @@ local good_notes =
 }
 
 function Test_chords:test_notes()
-   for _, n in ipairs(good_notes) do
+   for _, n in ipairs(notes) do
       local err, chord = chords.parse(n)
-      
+      assertIsNotNil(err)
+      --print(err, serpent.block(chord))
+      assertEquals(chord[2].note, n)
+   end
+end
+
+
+local numerals = 
+{
+   'I', 'II', 'III', 'IV', 'V', 'VI', 'VII',
+}
+
+function Test_chords:test_numerals()
+   for _, n in ipairs(numerals) do
+      local err, chord = chords.parse(n)
       --print(err, serpent.block(chord))
       assertEquals(chord[2].note, n)
    end
@@ -45,7 +59,36 @@ function Test_chords:test_qualities()
    for _, q in ipairs(qualities) do
       local err, chord = chords.parse(q)
       --print(serpent.block(chord))
-      assertEquals(chord[2].q, n)
+      assertEquals(chord[2].chord, n)
+   end
+end
+
+local sevenths = 
+{
+   'C7', 'CM7', 'Cmaj7', 'CMM7', 
+}
+
+function Test_chords:test_sevenths()
+   for _, a in ipairs(sevenths) do
+      local err, chord = chords.parse(a)
+      print(serpent.block(chord))
+      assertEquals(chord[2].chord, n)
+   end
+end
+
+
+local examples = 
+{
+   { 'C',    4, { 60, 64, 67 } },
+   { 'Cm',   4, { 60, 63, 67 } },
+}
+
+function Test_chords:test_tones()
+   for _, c in ipairs(examples) do
+      local chord_tones = chords.make_chord(c[1], c[2])
+      for i = 1, #c[3] do
+         assertEquals(c[3][i], chord_tones[i])
+      end
    end
 end
 
