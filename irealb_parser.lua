67887fb45762a9f0d33ab6bc0aa4c1ec062a4689
Stephen Irons <stephen.irons@clear.net.nz>
2013-04-25 10:44:37 +1200

    Added license details.

--- irealb_parser.lua
+++ irealb_parser.lua
@@ -1,4 +1,5 @@
-#! /usr/bin/env lua
+-- Copyright (c) 2013 Stephen Irons 
+-- License included at end of file
 
 local re = require('re')
 
@@ -190,4 +191,27 @@ M.unobfusc = obfusc
 M.transpose = 0
 
 return M
+
+-- Copyright (C) 2013 Stephen Irons
+-- 
+-- Permission is hereby granted, free of charge, to any person
+-- obtaining a copy of this software and associated documentation
+-- files (the "Software"), to deal in the Software without
+-- restriction, including without limitation the rights to use,
+-- copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the
+-- Software is furnished to do so, subject to the following
+-- conditions:
+-- 
+-- The above copyright notice and this permission notice shall be
+-- included in all copies or substantial portions of the Software.
+-- 
+-- The software is provided "as is", without warranty of any kind,
+-- express or implied, including but not limited to the warranties
+-- of merchantability, fitness for a particular purpose and
+-- noninfringement. In no event shall the authors or copyright
+-- holder be liable for any claim, damages or other liability,
+-- whether in an action of contract, tort or otherwise, arising
+-- from, out of or in connection with the software or the use or
+-- other dealings in the software.
 
