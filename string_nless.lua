59fd4cfb45447ac77ae7cc2bfca2e25bc9ce218d
Stephen Irons <stephen.irons@clear.net.nz>
2013-04-26 21:01:59 +1200

    Use LPEG to split string into components.

--- string_nless.lua
+++ string_nless.lua
@@ -17,9 +17,34 @@ local function string_nless_(a, b)
    end
 end
 
-function M.string_nless(a, b)
-   return string_nless_(a, b)
+local re = require 're'
+local pattern = re.compile([[
+   t <- s -> {}
+   s <- <word> <num> s? / <num> s? / <word> 
+   word <- { %D+ }
+   num  <- { %d+ }
+]])
+
+local function string_nsplit(s)
+   local n, t, a, b, c = re.find(s, pattern)
+   return t
 end
+
+local function string_nless(a, b)
+   local at = string_nsplit(a)
+   local bt = string_nsplit(b)
+   
+   for i = 1, math.min(#at, #bt) do
+--      print(at[i], bt[i])
+      if string_nless_(at[i], bt[i]) then
+         return true
+      end
+   end
+   return false
+end
+
+M.string_nsplit = string_nsplit
+M.string_nless = string_nless
 
 return M
 
--- test/test_string_nless.lua
+++ test/test_string_nless.lua
@@ -5,11 +5,13 @@
 
 package.path = './?/?.lua;' .. package.path
 require('luaunit')
+local serpent = require('serpent')
 
 local nless = require('string_nless')
 local snless = nless.string_nless
+local nsplit = nless.string_nsplit
 
-local tests = 
+local nless_tests = 
 {
    { 'a', 'a', false },
    { 'a', 'b', true  },
@@ -33,19 +35,60 @@ local tests =
    
    {  'a1', 'a10', true  },
    { 'a10',  'a1', false },
---   {  'a2', 'a10', true  },
---   { 'a10',  'a2', false },
+   {  'a2', 'a10', true  },
+   { 'a10',  'a2', false },
+   
+   { 'a1b', 'a1b', false },
+   { 'a1b', 'a1c', true  },
+   { 'a1b', 'a2b', true  },
+   { 'a1b', 'b1b', true  },
+
+   { 'a1b', 'a10b', true },
+   { 'a1b', 'a10c', true  },
+   { 'a2b', 'a10b', true  },
+   { 'a2b', 'a10c', true  },
+   
+   { '1.10', '1.100', true },
+   { '1.2' , '1.10' , true },
 }
 
 Test_nless = {}
 
-for i, t in ipairs(tests) do
+for i, t in ipairs(nless_tests) do
    local n = t[3] and '_true' or '_false'
    
    Test_nless['test_' .. t[1] .. '_lt_' .. t[2] .. n] = function()
       assertEquals(snless(t[1], t[2]),t[3]) 
    end 
 end
+
+
+
+
+local nsplit_tests = 
+{
+   { 'a',      { 'a'   } },
+   { 'ab',     { 'ab'  } },
+   { 'a1',     { 'a', '1' } },
+   { 'a12',    { 'a', '12' } },
+   { 'a12b',   { 'a', '12', 'b' } },
+   { 'a12bc3', { 'a', '12', 'bc', '3' } },
+   { '1'     , { '1' } },
+   { '12b',    { '12', 'b' } },
+   { '1.2b',   { '1' , '.', '2', 'b' } },
+}
+
+
+Test_nsplit = {}
+
+for i, t in ipairs(nsplit_tests) do
+   Test_nsplit['test_' .. t[1]] = function()
+      local a = nsplit(t[1])
+      local e = t[2]
+      assertEquals(a, e) 
+   end 
+end
+
 
 return LuaUnit:run()
 
